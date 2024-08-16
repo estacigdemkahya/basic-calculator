95fdb0f95c0ac3841899e467ed2b49459176c23e
Çiğdem Kahya <115181117+estacigdemkahya@users.noreply.github.com>
2024-08-16 14:51:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function appendValue(value){
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay(){
+  document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+// eval() fonksiyonu, JavaScript'te bir string ifadesini (metin olarak verilen kodu) değerlendirmek ve çalıştırmak için kullanılır.
+//   Bu fonksiyon, string olarak verilen bir JavaScript kodunu alır ve bu kodu çalıştırır. 
+//   Bu durumda, hesap makinesindeki işlemleri bu fonksiyon çözüyor.
 
